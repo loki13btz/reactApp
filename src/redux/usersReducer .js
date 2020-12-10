3e9704cc7b59e311fbe3287f512ba0c353f846de
Dasha <riotsoirit@gmail.com>
2020-12-10 19:21:03 +0200

    used Rest API,class component and componentDidMount

--- src/redux/usersReducer .js
+++ src/redux/usersReducer .js
@@ -3,39 +3,7 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 
 let initialState = {
-    users: [{
-            id: 1,
-            name: 'Ugen',
-            status: 'yo',
-            urlImg: 'https://i.pinimg.com/originals/64/a8/8f/64a88f80d6b5a43b58d14c20c7ef4b89.jpg',
-            followed: true,
-            location: { country: 'Ukraine', city: 'Kharkiv' }
-        },
-        {
-            id: 2,
-            name: 'Sasha',
-            status: 'lol',
-            urlImg: 'https://i.pinimg.com/originals/64/a8/8f/64a88f80d6b5a43b58d14c20c7ef4b89.jpg',
-            followed: false,
-            location: { country: 'Ukraine', city: 'Kharkiv' }
-        },
-        {
-            id: 3,
-            name: 'Dasha',
-            status: 'the best',
-            urlImg: 'https://i.pinimg.com/originals/64/a8/8f/64a88f80d6b5a43b58d14c20c7ef4b89.jpg',
-            followed: true,
-            location: { country: 'Ukraine', city: 'Kharkiv' }
-        },
-        {
-            id: 4,
-            name: 'Anna',
-            status: 'what are you want',
-            urlImg: 'https://i.pinimg.com/originals/64/a8/8f/64a88f80d6b5a43b58d14c20c7ef4b89.jpg',
-            followed: false,
-            location: { country: 'Ukraine', city: 'Kharkiv' }
-        }
-    ]
+    users: []
 }
 
 let usersReduser = (state = initialState, action) => {
