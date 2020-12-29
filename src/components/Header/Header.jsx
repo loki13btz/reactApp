11f977ac44bdb5d236503c133e2b5009c477dd5d
Dasha <riotsoirit@gmail.com>
2020-12-29 18:38:04 +0200

    added auth in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import './Header.css';
+import { NavLink } from 'react-router-dom';
+import s from './Header.module.css';
 
-const Header = () => {
+const Header = (props) => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img src='https://i.pinimg.com/originals/7c/45/9e/7c459e56fd8e93ee7b22ad7e7db94a54.png'></img>
+            <div className={s.loginBlock}>
+                {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+
+            </div>
         </header>
+
     );
 };
 
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,5 @@
 import { combineReducers, createStore } from "redux";
+import authReducer from "./authReducer";
 import dialogsReducer from "./dialogsReducer";
 import profileReducer from "./profileReducer";
 import usersReduser from "./usersReducer ";
@@ -6,7 +7,8 @@ import usersReduser from "./usersReducer ";
 let reducers = combineReducers({
     dialogsPage: dialogsReducer,
     profilePage: profileReducer,
-    UsersPage: usersReduser
+    UsersPage: usersReduser,
+    auth: authReducer
 })
 
 let store = createStore(reducers);
